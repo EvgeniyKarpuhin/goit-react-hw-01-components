fc6e2dd3bdc68090ff4c09cc400333bcf39d27f9
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-11-03 23:11:38 +0200

    Updates

---
+++ precache-manifest.a1547bd5737146d8b4f37a0818da6ca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43526e29f4aed7063a0eea9021e6803",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "54ba4085d9206273bcd6",
+    "url": "/goit-react-hw-01-components/static/css/main.1daa4c22.chunk.css"
+  },
+  {
+    "revision": "b4254a951c4e5f6e185c",
+    "url": "/goit-react-hw-01-components/static/js/2.8f027ee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.8f027ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ba4085d9206273bcd6",
+    "url": "/goit-react-hw-01-components/static/js/main.9d73508b.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
